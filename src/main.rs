16b3ce6dc07bea14f7944610d9722825d5fb1851
Oliver Wooding <oliverwooding@icloud.com>
2024-02-12 14:54:05 +0000

    core lexer functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//! A compiler for the IMP language.
+
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+
+pub mod lexer;
+pub mod parser;
+
 fn main() {
     println!("Hello, world!");
 }
