f028af0555c31299e8fe3461f9a47f19c2074e9b
Oliver Wooding <oliverwooding@icloud.com>
2024-04-10 12:12:08 +0200

    added crate::var module with Var struct

---
+++ src/var.rs
@@ -0,0 +1,30 @@
+//! A compiler-frontend [`Var`] type providing borrowed, copy-on-write semantics.
+
+use byteyarn::{YarnBox, YarnRef};
+
+/// A valid IMP variable, guaranteed to begin
+/// with a latin character and to have a body
+/// composed of latin characters, arabic numerals,
+/// and underscores.
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+pub struct Var<'src>(YarnBox<'src, str>);
+
+impl<'src> std::fmt::Display for Var<'src> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl<'src> From<&'src str> for Var<'src> {
+    fn from(value: &'src str) -> Self {
+        Self(YarnBox::from(value))
+    }
+}
+
+impl<'src> Var<'src> {
+    /// Returns a reference to the underlying [`YarnBox`].
+    pub fn get(&self) -> YarnRef<'_, str> {
+        self.0.as_ref()
+    }
+}
+
